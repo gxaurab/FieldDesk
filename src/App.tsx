98a7aff0707a1456830c69a8f186d1854e8f7e0a
gxaurab <oe.gaurab@gmail.com>
2026-07-14 19:06:28 +0545

    Initial proj setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='min-h-screen bg-black text-white'>
+      <h1> Hello new starter</h1>
+    </div>
+  )
+}
+
+export default App
